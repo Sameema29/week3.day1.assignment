96c2a41f2a0da0dd2e0d0db1e7f528000553e590
Sameema29 <109177227+Sameema29@users.noreply.github.com>
2022-07-24 01:10:03 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,14 @@
+package week3.day1.assignment.org.department;
+
+import week3.day1.assignment.org.college.College;
+
+public class Department extends College {
+	
+	public void departmentName()
+	{
+		System.out.println("Department Name is Maths");
+	}
+
+	
+
+}
