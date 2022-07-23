4dfa802e666205828db9349b444c75cef1767968
Sameema29 <109177227+Sameema29@users.noreply.github.com>
2022-07-24 01:09:37 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+package week3.day1.assignment.org.college;
+
+public class College {
+	
+	public void collegeName()
+	{
+		System.out.println("College Name is ABC");
+	}
+	
+	public void collegeCode()
+	{
+		System.out.println("College Code is 123");
+	}
+
+	public void collegeRank()
+	{
+		System.out.println("College Rank is 3");
+	}
+}
