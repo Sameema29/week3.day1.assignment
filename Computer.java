9c1d28b26b7be6c70a42b358743e9fd0695a4827
Sameema29 <109177227+Sameema29@users.noreply.github.com>
2022-07-24 01:10:56 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,9 @@
+package week3.day1.assignment.org.system;
+
+public class Computer {
+
+	public void computerModel()
+	{
+		System.out.println("Computer Model is HP");
+	}
+}
---
+++ Desktop.java
@@ -0,0 +1,12 @@
+package week3.day1.assignment.org.system;
+
+public class Desktop extends Computer{
+	
+	
+	public void desktopSize()
+	{
+		super.computerModel();
+		System.out.println("Desktop size is 5");
+	}
+
+}
